f2d130985a7b2a68da14fd976aab326fad67ad99
SolaTyolo <solat.yolo@gmail.com>
2018-02-13 21:49:36 +0800

    modify
    整体修改，使用compose

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # self-Dockerfile
+
+2-10 将配置文件全部单独配置
--- lnmp/mysql/mysql.sh
+++ lnmp/mysql/mysql.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 docker build --tag mysql-server -f mysql/Dockerfile .
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -it mysql-server
-
-#-v /usr/local/root/bo/data/mysql:/var/lib/mysql
+docker run --name mysql -p 3306:3306 -v ~/opt/data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -it mysql-server
