be4955336a7559b7db4dcba53b287d3b5fde2639
SolaTyolo <solat.yolo@gmail.com>
2018-02-15 11:19:37 +0800

    debug
    修改ro 报错

--- lnmp/cli.sh
+++ lnmp/cli.sh
@@ -1,5 +1,5 @@
 #bin/bash
 
-alias composer-cli="docker run --privileged=true --volumes-from data --rm php-composer"
+alias composer-cli="docker run --privileged=true --volumes-from lnmp_data_1 --rm composer"
 
-alias artisan-cli="$docker run --privileged=true --volumes-from data --rm php-artisan"
+alias artisan-cli="docker run --privileged=true --volumes-from lnmp_data_1 --rm lnmp_php-artisan"
--- lnmp/php-composer/composer.sh
+++ lnmp/php-composer/composer.sh
@@ -1,5 +1,5 @@
 #bin/bash
-
-docker build --tag php-composer -f php-composer/Dockerfile .
+#采用官方镜像
+#docker build --tag php-composer -f php-composer/Dockerfile .
 
 #$docker run --privileged=true --volumes-from data --rm php-composer
